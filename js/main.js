bc161ffde4185eb744345a95bcf94759572ac723
thanapolk <thanapol.k@entronica.co.th>
2019-01-27 16:17:20 +0700

    update

--- js/main.js
+++ js/main.js
@@ -30,3 +30,33 @@ data =
 }
 
 new Chart(CHART , data )
+
+CHART = document.getElementById("lineChart1");
+
+data =
+{
+    type: 'pie',
+    data:
+    {
+        labels: ['1','2','3','4'],
+        datasets:[
+            {
+                label : 'Points',
+                backgroundColor:['#f1c40f','#e67e22','#16a085','#2980b9'],
+                data : [50,30,10,10]
+
+
+            }
+        ]
+    }
+    ,
+    options :{
+        cutoutPercentage : 60,
+        rotation:Math.PI* -0.5,
+        animation:{
+            animateScale: true
+        }
+    }
+}
+
+new Chart(CHART , data )
