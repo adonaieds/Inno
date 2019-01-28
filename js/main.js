9dc727a2fc7135bdf81f551cd7865c95bdf7442f
thanapolk <thanapol.k@entronica.co.th>
2019-01-28 20:40:28 +0700

    up

--- js/main.js
+++ js/main.js
@@ -2,73 +2,31 @@ let CHART = document.getElementById("lineChart");
 // Chart.defaults.scale.ticks.beginAtZero = true;
 
 let data
-
-
-
 data =
-{
-    type: 'pie',
-    data:
     {
-        labels: ['1','2'],
-        datasets:[
-            {
-                label : 'Points',
-                backgroundColor:['#01EDE1','#3F3F3F'],
-                data : [60,40]
-
-
+        type: 'pie',
+        data:
+        {
+            labels: ['1', '2'],
+            datasets: [
+                {
+                    label: 'Points',
+                    backgroundColor: ['#01EDE1', '#3F3F3F'],
+                    data: [60, 40]
+                }
+            ]
+        }
+        ,
+        options: {
+            cutoutPercentage: 60,
+            rotation: Math.PI * -0.5,
+            animation: {
+                animateScale: true
+            },
+            legend: {
+                display: false
             }
-        ]
-    }
-    ,
-    options :{
-        cutoutPercentage : 60,
-        rotation:Math.PI* -0.5,
-        animation:{
-            animateScale: true
-        },
-        legend: {
-            display: false
         }
     }
-}
 
-new Chart(CHART , data )
-
-
-
-
-let CHART1 = document.getElementById("lineChart1");
-// Chart.defaults.scale.ticks.beginAtZero = true;
-
-data =
-{
-    type: 'pie',
-    data:
-    {
-        labels: ['1','2'],
-        datasets:[
-            {
-                label : 'Points',
-                backgroundColor:['#01EDE1','#3F3F3F'],
-                data : [60,40]
-
-
-            }
-        ]
-    }
-    ,
-    options :{
-        cutoutPercentage : 60,
-        rotation:Math.PI* -0.5,
-        animation:{
-            animateScale: true
-        },
-        legend: {
-            display: false
-        }
-    }
-}
-
-new Chart(CHART1 , data )
+new Chart(CHART, data)
